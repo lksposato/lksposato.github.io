ce29ee653c3e472bb7f2cd1ac9b1774ae4a0fc13
BillDoberman <don.j.larson@gmail.com>
2023-05-03 20:30:22 -0700

    stuff

--- titration/script.js
+++ titration/script.js
@@ -3,6 +3,18 @@ var drag_source = null;
 
 const HOVER_COLOR = '#aaaaaa'
 const DROPZONE_COLOR = '#3d424d'
+const DROPZONE_SIZE = '200px'
+const IMAGE_SIZE = "130px"
+const IMAGE_HOVER_SIZE = "140px"
+
+function mouseover(event) {
+	event.target.style.width = IMAGE_HOVER_SIZE
+}
+
+function mouseleave(event) {
+	console.log("mouse leave")
+	event.target.style.width = IMAGE_SIZE
+}
 
 function dragstart(event) {
 	console.log("drag start", event.target)
@@ -16,7 +28,9 @@ function dragexit(e) {
 
 function dragover(e) {
 	console.log(e.target.style);
-	e.target.style.backgroundColor = HOVER_COLOR;
+	if (e.target.childElementCount == 0) {
+		e.target.style.backgroundColor = HOVER_COLOR;
+	}
 	e.preventDefault();
 }
 
@@ -24,13 +38,18 @@ function dragdrop(e) {
 	e.preventDefault();
 	if (e.target.className == "dropzone") {
 		e.target.style.backgroundColor = DROPZONE_COLOR;
-		e.target.appendChild(drag_source)
-	}
+		if (e.target.childElementCount == 0) {
+			e.target.appendChild(drag_source)
+		}
+	}	
 }
 
 var draggables = document.querySelectorAll('[draggable="true"]');
 draggables.forEach(function(element) {
+	element.addEventListener('mouseover', mouseover)
+	element.addEventListener('mouseleave', mouseleave)
 	element.addEventListener('dragstart', dragstart)
+	element.style.width = IMAGE_SIZE
 })
 
 var dropzones = Array.from(document.getElementsByClassName('dropzone'))
@@ -39,4 +58,7 @@ dropzones.forEach(function(element) {
 	element.addEventListener('dragover', dragover)
 	element.addEventListener('dragleave', dragexit)
 	element.style.backgroundColor = DROPZONE_COLOR
+	element.style.width = DROPZONE_SIZE
+	element.style.height = DROPZONE_SIZE
 })
+
