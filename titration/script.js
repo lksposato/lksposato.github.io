cb97cd92dcadb747a4ce0a337d18c0d1f688511f
BillDoberman <don.j.larson@gmail.com>
2023-05-03 18:54:48 -0700

    noice

--- titration/script.js
+++ titration/script.js
@@ -2,7 +2,7 @@ var debuglog = document.getElementById('debuglog');
 var drag_source = null;
 
 function dragstart(event) {
-	console.log(event.target)
+	console.log("drag start", event.target)
 	drag_source = event.target;
 }
 
@@ -12,19 +12,17 @@ function dragover(e) {
 
 function dragdrop(e) {
 	e.preventDefault();
-	let image = e.dataTransfer.getData('text/html');
-	drag_source.remove();
-	e.target.innerHTML = image;
+	if (e.target.className == "dropzone") {
+		e.target.appendChild(drag_source)
+	}
 }
 
 var draggables = document.querySelectorAll('[draggable="true"]');
 draggables.forEach(function(element) {
-	console.log("event addeed")
 	element.addEventListener('dragstart', dragstart)
 })
 
 var dropzones = Array.from(document.getElementsByClassName('dropzone'))
-console.log(dropzones)
 dropzones.forEach(function(element) {
 	element.addEventListener('drop', dragdrop)
 	element.addEventListener('dragover', dragover)
